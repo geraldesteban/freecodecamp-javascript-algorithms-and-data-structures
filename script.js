ae6c76e8de46687c73c978d00c283262ddc56eb9
geraldesteban <wakin.esteban@gmail.com>
2023-10-11 20:13:53 +0800

    add 3rd project caesars cipher

--- script.js
+++ script.js
@@ -96,3 +96,21 @@ const convertToRoman = function (num) {
   return result;
 };
 console.log(convertToRoman(3999));
+
+// 3rd Project: Caesars Cipher
+/* Data Test:
+rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+rot13("SERR YBIR?") should decode to the string FREE LOVE?
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+*/
+
+const rot13 = function (str) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alphaRot13 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+  return str.replace(/[A-Za-z]/g, function (char) {
+    const i = alphabet.indexOf(char);
+    return alphaRot13[i];
+  });
+};
+console.log(rot13("SERR PBQR PNZC"));
