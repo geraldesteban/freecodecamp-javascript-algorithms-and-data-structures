a912c2f485b92c7756279dad95cc6a6bf4289e9d
geraldesteban <wakin.esteban@gmail.com>
2023-10-12 22:44:38 +0800

    add cash register project

--- script.js
+++ script.js
@@ -154,3 +154,78 @@ const telephoneCheck = function (str) {
   return validUSPhoneNumber.test(str);
 };
 console.log(telephoneCheck("555-555-5555"));
+
+// 5th Project: Cash Register
+/* Data Test:
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
+(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+*/
+
+const checkCashRegister = function (price, cash, cid) {
+  const currencyUnits = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1],
+    ["FIVE", 5],
+    ["TEN", 10],
+    ["TWENTY", 20],
+    ["ONE HUNDRED", 100],
+  ];
+
+  let changeDue = cash - price;
+  let totalCID = 0;
+  let change = [];
+
+  for (const [, val] of cid) {
+    totalCID += val;
+  }
+
+  if (totalCID < changeDue) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (totalCID === changeDue) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    for (let i = currencyUnits.length - 1; i >= 0; i--) {
+      const [unit, unitValue] = currencyUnits[i];
+      const CIDAmounts = cid[i][1];
+      const maxUnits = Math.floor(CIDAmounts / unitValue);
+      const unitsToGive = Math.min(maxUnits, Math.floor(changeDue / unitValue));
+      const amountToDeduct = unitsToGive * unitValue;
+
+      if (unitsToGive > 0) {
+        change.push([unit, amountToDeduct]);
+        changeDue -= amountToDeduct;
+        changeDue = Number(changeDue.toFixed(2));
+      }
+    }
+
+    if (changeDue === 0) {
+      if (totalCID === change.reduce((acc, [, value]) => acc + value, 0)) {
+        return { status: "CLOSED", change: cid };
+      } else {
+        return { status: "OPEN", change };
+      }
+    } else {
+      return { status: "INSUFFICIENT_FUNDS", change: [] };
+    }
+  }
+};
+console.log(
+  checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
